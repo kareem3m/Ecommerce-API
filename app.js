bb7cc9b7022e0f8bd8e3c7167c49ff063504b1a6
kareem3m <kareem.mohamed9711@gmail.com>
2022-10-26 01:32:07 +0200

    removed unused code

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require("express");
+const logger = require("morgan");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+require("./config/database").connect();
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+// Logic goes here
 
 module.exports = app;
