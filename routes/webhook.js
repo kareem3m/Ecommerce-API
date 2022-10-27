d963d9da0c0897e9a405c8a9287a556218613c58
kareem3m <kareem.mohamed9711@gmail.com>
2022-10-27 14:53:11 +0200

    webhook update db

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,39 +1,40 @@
-
-const stripe = require('stripe')(process.env.STRIPE_API_KEY);
-
-const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
-
 const express = require("express");
 const router = express.Router();
+const Order = require("../models/order");
+const bodyParser = require("body-parser");
 
-const bodyParser = require('body-parser');
+const stripe = require("stripe")(process.env.STRIPE_API_KEY);
+const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
-const fulfillOrder = (session) => {
-  console.log("Fulfilling order", session);
-}
+router.post(
+  "/webhook",
+  bodyParser.raw({ type: "application/json" }),
+  (request, response) => {
+    const payload = request.body;
+    const sig = request.headers["stripe-signature"];
 
-router.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, response) => {
-  const payload = request.body;
-  const sig = request.headers['stripe-signature'];
+    let event;
 
-  let event;
+    try {
+      event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+    } catch (err) {
+      console.log(err.message);
+      return response.status(400).send(`Webhook Error: ${err.message}`);
+    }
 
-  try {
-    event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
-  } catch (err) {
-    console.log(err.message);
-    return response.status(400).send(`Webhook Error: ${err.message}`);
-  }
-
-  // Handle the checkout.session.completed event
-  if (event.type === 'checkout.session.completed') {
     const session = event.data.object;
 
-    // Fulfill the purchase...
-    fulfillOrder(session);
-  }
+    if (event.type === "checkout.session.completed") {
+      Order.updateOne({ paymentSessionId: session.id }, { status: "paid" });
+    } else {
+      Order.updateOne(
+        { paymentSessionId: session.id },
+        { status: "paymentFailed" }
+      );
+    }
 
-  response.status(200);
-});
+    response.status(200);
+  }
+);
 
 module.exports = router;
